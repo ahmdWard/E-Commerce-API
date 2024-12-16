06e7d1418915c3447fd0f83835b73e1b0f759d69
ahmdWard <ahmdemadabdallah@gmail.com>
2024-12-16 21:22:51 +0200

    Add root route to send a response from the server

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ dotenv.config()
 
 const port = process.env.PORT || 3000;
 
+app.use ('/',(req,res)=>{
+    res.send("Hello from server side")
+})
 app.listen(port,()=>{
     console.log(`server is running at port ${port} 🚀🚀`)
 })
