a60344a892d2a5665d7d24838924aaab727eec5e
ahmdWard <ahmdemadabdallah@gmail.com>
2024-12-17 23:44:06 +0200

    creating user

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,9 +6,21 @@ exports.getAllUsers  = async (req,res,next)=>{
 
     res.status(200).json({
         status:"success",
+        length:users.length,
         data:{
             users
         }
     })
 }
 
+exports.createUser = async (req,res,next)=>{
+    
+    const user = await User.create(req.body)
+
+    res.status(201).json({
+        status:"success",
+        data:{
+             user
+        }
+    })
+}
