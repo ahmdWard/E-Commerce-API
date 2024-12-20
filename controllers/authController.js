299791fb3f132ab05ef87620f6da61cce422c33f
ahmdWard <ahmdemadabdallah@gmail.com>
2024-12-21 00:16:58 +0200

    adding tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 const AppError = require('../utilts/appError')
 const catchAsync = require('../middleware/catchAsync')
@@ -10,7 +11,8 @@ exports.login = catchAsync(async(req,res,next)=>{
     if (!email || !password) {
         return next(new AppError('Please provide email and password', 400));
     }
-    const user = await User.findOne({ email })
+    
+    const user = await User.findOne({ email }).select("+password")
     if(!user)
        return next(new AppError('Email or password is not correct',403))
 
@@ -18,9 +20,14 @@ exports.login = catchAsync(async(req,res,next)=>{
     if(!user.comparePassword(password,user.password))
       return  next(new AppError('Email or password is not correct',403))
 
+    const token = jwt.sign({ id:user.id},process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_EXPIRATION
+       })  
+
     res.status(200).json({
         status:"success",
-        message: 'Logged in successfully'
+        message: 'Logged in successfully',
+        token
     })
 
 })
