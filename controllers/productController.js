1192d0ccb45da28c3dc9a92f07f8fd77c4ab7f24
ahmdWard <ahmdemadabdallah@gmail.com>
2025-02-16 23:49:11 +0200

    feat: adding handler factory

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,101 +2,32 @@ const catchAsync = require('../middleware/catchAsync')
 const appError = require('../utilts/appError')
 const Product = require('../models/productModel')
 const ApiFeatures = require('../utilts/apiFeatures')
-
+const factory = require('../controllers/handlerFactory')
 
 // @desc   CREATE product
 // @route POST /api/v1/products
 // @access Public
 
-exports.createProduct = catchAsync(async(req,res,next)=>{
-
-    const newProduct = await Product.create(req.body)
-
-    res.status(201).json({
-        status:"success",
-        data:{
-            newProduct
-        }
-    })
-})
+exports.createProduct = factory.createOne(Product)
 
 // @desc   Get list of products
 // @route GET /api/v1/products
 // @access Public
 
-exports.getAllProducts = catchAsync(async (req, res, next) => {
-    const documentCounts = await Product.countDocuments();
-    
-    const features = new ApiFeatures(Product.find(), req.query)
-    .search()
-    .filter()
-    .sort()
-    .paginate(documentCounts)
-    .limitFields();
-
-    const products = await features.moongoseQuery; 
-
-    res.status(200).json({
-        status: 'success',
-        length: products.length,
-        data: {
-            products
-        }
-    });
-});
+exports.getAllProducts = factory.getAll(Product ,"product")
 
 
 // @desc   Get specific product by id
 // @route GET /api/v1/products/:id
 // @access Public
-exports.getProduct = catchAsync(async(req,res,next)=>{
-
-    const product = await Product.findById(req.params.id)
-
-    if(!product)
-        return next(new appError('product is not found',404))
-
-    res.status(200).json({
-        status:'success',
-        data:{
-            product
-        }
-    })
-})
+exports.getProduct = factory.getOne(Product)
 
 // @desc   UPDATE specific product by id
 // @route Patch /api/v1/products/:id
 // @access Public
-exports.updateProduct = catchAsync(async(req,res,next)=>{
-
-    const product = await Product.findByIdAndUpdate(req.params.id,req.body,{
-        runValidators:true,
-        new:true
-    })
-    
-    if(!product)
-        return next(new appError('product is not found 404',404))
-
-    res.status(200).json({
-        status:'success',
-        data:{
-            product
-        }
-    })
-})
-
+exports.updateProduct = factory.updateOne(Product)
 // @desc   DELETE specific product by id
 // @route DELETE /api/v1/products/:id
 // @access Public
 
-exports.deleteProduct = catchAsync(async(req,res,next)=>{
-
-    const product = await Product.findByIdAndDelete(req.params.id)
-
-    if(!product)
-        return next(new appError('product is not found 404',404))
-
-    res.status(204).json({
-        status:'success'
-    })
-})
+exports.deleteProduct = factory.deleteOne(Product)
