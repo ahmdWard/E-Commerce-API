12a5e64a35895f1b10bc7b17f4f891234ac90845
ahmdWard <ahmdemadabdallah@gmail.com>
2025-02-16 20:17:17 +0200

    feat: adding api features (searching filtering limit pagination)

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,12 @@
 const catchAsync = require('../middleware/catchAsync')
 const appError = require('../utilts/appError')
 const Product = require('../models/productModel')
+const ApiFeatures = require('../utilts/apiFeatures')
+
+
+// @desc   CREATE product
+// @route POST /api/v1/products
+// @access Public
 
 exports.createProduct = catchAsync(async(req,res,next)=>{
 
@@ -14,19 +20,35 @@ exports.createProduct = catchAsync(async(req,res,next)=>{
     })
 })
 
-exports.getAllProducts = catchAsync(async(req,res,next)=>{
+// @desc   Get list of products
+// @route GET /api/v1/products
+// @access Public
 
-    const products = await Product.find()
+exports.getAllProducts = catchAsync(async (req, res, next) => {
+    const documentCounts = await Product.countDocuments();
+    
+    const features = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .sort()
+    .paginate(documentCounts)
+    .limitFields();
+
+    const products = await features.moongoseQuery; 
 
     res.status(200).json({
-        status:'success',
-        length:products.length,
-        data:{
+        status: 'success',
+        length: products.length,
+        data: {
             products
         }
-    })
-})
+    });
+});
 
+
+// @desc   Get specific product by id
+// @route GET /api/v1/products/:id
+// @access Public
 exports.getProduct = catchAsync(async(req,res,next)=>{
 
     const product = await Product.findById(req.params.id)
@@ -42,6 +64,9 @@ exports.getProduct = catchAsync(async(req,res,next)=>{
     })
 })
 
+// @desc   UPDATE specific product by id
+// @route Patch /api/v1/products/:id
+// @access Public
 exports.updateProduct = catchAsync(async(req,res,next)=>{
 
     const product = await Product.findByIdAndUpdate(req.params.id,req.body,{
@@ -59,6 +84,10 @@ exports.updateProduct = catchAsync(async(req,res,next)=>{
         }
     })
 })
+
+// @desc   DELETE specific product by id
+// @route DELETE /api/v1/products/:id
+// @access Public
 
 exports.deleteProduct = catchAsync(async(req,res,next)=>{
 
