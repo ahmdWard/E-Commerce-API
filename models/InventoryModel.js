1a49c8c20af7b82a26a106f3020615429daf05aa
ahmdWard <ahmdemadabdallah@gmail.com>
2025-02-19 20:34:16 +0200

    feat:adding inventory routes , model & controller

---
+++ models/InventoryModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const inventorySchema = new mongoose.Schema({
+
+    product:{
+        type:mongoose.Schema.ObjectId,
+        ref:"Product",
+        required:true,
+    },
+    stock:{
+        type:Number,
+        require:true,
+        min:0
+    },
+    reservedStock:{
+        type:Number,
+        default:0,
+        min:0
+    },
+    sold:{
+        type:Number,
+        default:0,
+        min:0
+    },
+    restocks:[
+        {
+            quantity:Number,
+            date:{
+                type:Date,
+                default:Date.now
+            }
+        }
+    ],
+    lastUpdated:{
+        type:Date,
+        default:Date.now
+    }
+
+})
+
+// inventory tracking and stock points 
+
+const inventory = mongoose.model('Inventory',inventorySchema)
+
+module.exports = inventory
