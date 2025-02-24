4d7dc749abe01c4535d2fe37a0a16ad2972754b5
ahmdWard <ahmdemadabdallah@gmail.com>
2025-02-24 21:25:50 +0200

    feat:adding shipping model

---
+++ models/shipmentModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const shippingSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User',
+        required:true
+    },
+    order:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Order',
+        required:true
+    },
+    status:{
+        type:String,
+        enum:['pending,delivered,shipped']
+    },
+    address:[
+        {
+            country:{
+                type:String,
+                required:true
+            },
+            state:{
+                type:String,
+                required:true
+            },
+            street:{
+                type:String,
+                required:true
+            },
+            building:{
+                type:Number,
+                required:true
+            },
+            flatNumber:{
+                type:Number,
+                required:true
+            }
+        }
+    ]
+},{timestamps:true})
+
+
+const shipping = mongoose.model('Shipping',shippingSchema)
+module.exports = shipping
